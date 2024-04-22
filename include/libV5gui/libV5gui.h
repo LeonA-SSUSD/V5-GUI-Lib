053b69f58ed976ae002a369f3c9016e0913a2acf
ryatoast <reactasia@outlook.com>
2024-04-22 07:47:01 -0700

    Showed all includes in libv5gui.h rather than the integral ones

--- include/libV5gui/libV5gui.h
+++ include/libV5gui/libV5gui.h
@@ -1,7 +1,10 @@
 #ifndef LIBV5GUI_H
 #define LIBV5GUI_H
 
+#include "libv5gui/core/ScreenElement.h"
 #include "libv5gui/core/Screen.h"
+#include "libv5gui/core/Text.h"
+#include "libv5gui/core/ButtonElement.h"
 
 #include "libv5gui/buttons/RectButton.h"
 #include "libV5gui/buttons/CircButton.h"
