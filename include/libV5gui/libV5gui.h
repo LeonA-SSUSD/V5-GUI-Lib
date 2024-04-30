a896cc3c8a7732672c46818ce8504e546c607f1d
ryatoast <reactasia@outlook.com>
2024-04-30 15:36:21 -0700

    Added CoreFeatures to main header

--- include/libV5gui/libV5gui.h
+++ include/libV5gui/libV5gui.h
@@ -1,10 +1,11 @@
 #ifndef LIBV5GUI_H
 #define LIBV5GUI_H
 
+#include "libV5gui/core/CoreFeatures.h"
 #include "libv5gui/core/ScreenElement.h"
 #include "libv5gui/core/Screen.h"
-#include "libV5gui/core/Shape.h"
 #include "libv5gui/core/Text.h"
+#include "libV5gui/core/Shape.h"
 #include "libv5gui/core/ButtonElement.h"
 
 #include "libv5gui/shapes/Rectangle.h"
