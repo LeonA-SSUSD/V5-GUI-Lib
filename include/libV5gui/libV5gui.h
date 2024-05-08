d4f513088ca4838ba869a8156ed87b4404ed101b
ryatoast <reactasia@outlook.com>
2024-05-08 09:39:43 -0700

    Name case consistency and added EventThreader header and AutonomousSelector header

--- include/libV5gui/libV5gui.h
+++ include/libV5gui/libV5gui.h
@@ -2,17 +2,20 @@
 #define LIBV5GUI_H
 
 #include "libV5gui/core/CoreFeatures.h"
-#include "libv5gui/core/ScreenElement.h"
+#include "libV5gui/core/EventThreader.h"
+#include "libV5gui/core/ScreenElement.h"
 #include "libV5gui/core/ColorElement.h"
-#include "libv5gui/core/Text.h"
-#include "libv5gui/core/Screen.h"
+#include "libV5gui/core/Text.h"
+#include "libV5gui/core/Screen.h"
 #include "libV5gui/core/Shape.h"
-#include "libv5gui/core/ButtonElement.h"
+#include "libV5gui/core/ButtonElement.h"
 
-#include "libv5gui/shapes/Rectangle.h"
+#include "libV5gui/shapes/Rectangle.h"
 #include "libV5gui/shapes/Circle.h"
 
-#include "libv5gui/buttons/RectButton.h"
+#include "libV5gui/buttons/RectButton.h"
 #include "libV5gui/buttons/CircButton.h"
+
+#include "libV5gui/comp/AutonomousSelector.h"
 
 #endif
